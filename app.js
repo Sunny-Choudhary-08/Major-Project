0bbc93024032bedeca0c2b6f93f0f85f3993467f
Sunny Choudhary <sunnytomar8218@gmail.com>
2024-10-20 14:13:35 +0530

    phase 1 completed

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-
+const {listingSchema} = require("./schema.js");
 
 
 
@@ -44,6 +44,20 @@ app.get("/listings", wrapAsync(async (req, res) => {
   res.render("listings/index.ejs", { allListings });
 }));
 
+//joi schema middleware
+
+const validateListing = (req, res, next) => {
+  
+  let {error} = listingSchema.validate(req.body);
+  if(error) {
+    let errorMessage = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400 , error.message);
+  } else {
+    next();
+  }
+}
+
+
 //New Route
 app.get("/listings/new", (req, res) => {
   res.render("listings/new.ejs");
@@ -59,11 +73,8 @@ app.get("/listings/:id" , wrapAsync(async(req ,res) => {
 
 //create route to add new listing 
 
-app.post("/listings" , wrapAsync(async(req,res ,next) => {
+app.post("/listings" ,validateListing, wrapAsync(async(req,res ,next) => {
 
-  if(!req.body.listing) {
-    throw new ExpressError(400 , "Send valid data for listing");
-  }
   const newListing =  new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
@@ -80,7 +91,7 @@ app.get("/listings/:id/edit" , wrapAsync(async(req ,res) => {
 
 // update route to save edit route data
 
-app.put("/listings/:id" , wrapAsync(async(req,res) => {
+app.put("/listings/:id" , validateListing , wrapAsync(async(req,res) => {
   let {id} =req.params;
   await Listing.findByIdAndUpdate(id , {...req.body.listing});
   res.redirect(`/listings/${id}`);
