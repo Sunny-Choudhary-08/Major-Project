97b2afc48deaaec7773c5b01ce09d294c8302304
Sunny Choudhary <sunnytomar8218@gmail.com>
2024-12-06 23:27:41 +0530

    working fine now

--- app.js
+++ app.js
@@ -134,11 +134,18 @@ app.post("/listings/:id/reviews" , validatereview , wrapAsync(async (req,res) =>
 
 // delete reviw route
 
+// app.delete("/listings/:id/reviews/:reviewId" , wrapAsync(async(req,res) => {
+//   let { id , reviewId } =  req.params;
+//   await Listing.findByIdAndUpdate(id , {$pull : {reviews : reviewId}})
+//   await Review.findByIdAndDelete(reviewId);
+
+//   res.redirect(`/listings/${id}`);
+// }))
+
 app.delete("/listings/:id/reviews/:reviewId" , wrapAsync(async(req,res) => {
   let { id , reviewId } =  req.params;
   await Listing.findByIdAndUpdate(id , {$pull : {reviews : reviewId}})
   await Review.findByIdAndDelete(reviewId);
-
   res.redirect(`/listings/${id}`);
 }))
 
